e80aa5b3d67b18285086250f8b5bf40ede717f3c
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-03 12:09:57 +0200

    Set uid and define login and logout method

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
+
 import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  uid = of('123');
+  //uid = of('123');
+  uid = this.authenticator.authState.pipe(
+    map(authState => {
+      if (authState) {
+        return authState.uid;
+      } else {
+        return null;
+      }
+    })
+  );
   isAdmin = of(true);
 
   constructor(
-    private adAuth: AngularFireAuth
+    private authenticator: AngularFireAuth
   ) { }
 
   login() {
-
+    this.authenticator.auth.signInWithPopup( new auth.GoogleAuthProvider() );
   }
 
-  Logout() {
-    
+  logout() {
+    this.authenticator.auth.signOut();
   }
   
 }
